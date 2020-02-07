48c5c4f9fcd15bde264884d2e7982c116d3debf1
khoanguyen9795 <khoanguyen438@gmail.com>
2020-02-07 21:35:34 +0700

    random finish

--- app/src/main/java/khoanguyen/com/random/MainActivity.java
+++ app/src/main/java/khoanguyen/com/random/MainActivity.java
@@ -14,13 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // cách 1 : Dùng lớp random
-//        Random random = new Random();
-
+        Random random = new Random();
+        for ( int i =0; i<100; i++){
+            // công thức hàm random ( max-min+1) + min)
+        int value =75 +(random.nextInt(133-75+1));
+        Log.d ("BBB", value+ "");}
+        // dấu + có 2 chức năng nếu cùng kiểu dự liệu số sẽ là cộng
+        // nếu đg trong 1 string sẽ là phép nối chuõi
         // cách 2 : Math
-        double numberRandom = Math.random();
-        Log.d ("BBB", String.valueOf(numberRandom));
-        long value = Math.round(numberRandom);
-        Log.d ("BBB", String.valueOf(value));
+//        for ( int i = 0; i<100 ; i++){
+//        double numberRandom = (Math.random()*4)+5;
+////        Log.d ("BBB", String.valueOf(numberRandom));
+//        long value = Math.round(numberRandom);
+//        Log.d ("BBB", String.valueOf(value));}
         // 1/ phím tắt hỗ trợ fix lỗi : alt + enter
         // 2/ Xem cách fix tren stackoverload
         // 3/ coi document của object
