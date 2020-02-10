04fc43aab081e304b83e025e47eab2f0728d5d61
khoanguyen9795 <khoanguyen438@gmail.com>
2020-02-10 20:50:31 +0700

    syntax game random

--- app/src/main/java/khoanguyen/com/random/MainActivity.java
+++ app/src/main/java/khoanguyen/com/random/MainActivity.java
@@ -4,32 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mEdtSomin, mEdtSomax;
+    Button mBtnRandom;
+    TextView mTvKetqua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // cách 1 : Dùng lớp random
-        Random random = new Random();
-        for ( int i =0; i<100; i++){
-            // công thức hàm random ( max-min+1) + min)
-        int value =75 +(random.nextInt(133-75+1));
-        Log.d ("BBB", value+ "");}
-        // dấu + có 2 chức năng nếu cùng kiểu dự liệu số sẽ là cộng
-        // nếu đg trong 1 string sẽ là phép nối chuõi
-        // cách 2 : Math
-//        for ( int i = 0; i<100 ; i++){
-//        double numberRandom = (Math.random()*4)+5;
-////        Log.d ("BBB", String.valueOf(numberRandom));
-//        long value = Math.round(numberRandom);
-//        Log.d ("BBB", String.valueOf(value));}
-        // 1/ phím tắt hỗ trợ fix lỗi : alt + enter
-        // 2/ Xem cách fix tren stackoverload
-        // 3/ coi document của object
+       mEdtSomax = findViewById(R.id.editSomax);
+       mEdtSomin = findViewById(R.id.editSomin);
+       mBtnRandom = findViewById(R.id.btnRandom);
+       mTvKetqua = findViewById(R.id.tvKetqua);
+        // Task 1 : Kiểm tra có dữ liệu trong 2 Edittext hay không
+        // Task 2 : Nếu số max < số min , số max = smin + 1;
+        // Task 3 : Click button random trả về số trong khoản min và max
+        // Task 4 : Nối chuỗi dữ liệu các giá trị random (5 - 3 - 2 - 1 -)
+        mBtnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textSmin = mEdtSomin.getText().toString();
+                String textSmax = mEdtSomax.getText().toString();
 
+                Log.d("BBB",textSmax);
+                Log.d("BBB",textSmin);
+            }
+        });
     }
 }
